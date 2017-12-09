41b1c2eabcc4e8d4ab47e11b6f5d74de32a519e4
Chris <christophersstjohn@gmail.com>
2017-12-09 14:43:50 -0800

    Working for test file

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-const express = require('express');
 const fs = require('fs');
 const http = require('http');
+const express = require('express');
 const app = express();
 const parser = require('xml2json');
 
-var xml = fs.readFileSync('short-data/US20170351222A1-20171207.XML','utf-8');
+app.get('/patent/:filenum\.json',function (req,res) {
+    // Browser: http://localhost:3090/patent.json
 
-xml = xml.replace(/\"/g,'\"');
-xml = xml.replace(/\'/g,"\'");
+    var xml = fs.readFileSync('data/US20170351222A1-20171207.XML','utf-8');
+    
+    xml = xml.replace(/\"/g,'\"'); 
+    xml = xml.replace(/\'/g,"\'");
 
-console.log(xml);
-
-
-// xml to json 
-var json = parser.toJson(xml);
-
-
-app.get('/test.json',function (req,res) {
-    // http://localhost:3090/test.json
+    // convert XML to JSON
+    var json = parser.toJson(xml); 
 
     res.setHeader("Content-Type", "application/json");
     res.send(`${json}`);
-});
+}); 
 // Server Setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
